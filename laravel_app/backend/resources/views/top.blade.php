aa3c6e2be46983e21fb0280a3388fe0cbdd98521
{ryosuke27} <opu.7455@gmail.com>
2020-10-02 06:41:14 +0900

    トップ画面

---
+++ laravel_app/backend/resources/views/top.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<header>
+	<h1>Visualize Manpower</h1>
+	<nav>
+		<a href="/">トップ</a>
+	</nav>
+</header>
+<main>
+	<h1>Visualize Manpower</h1>
+</main>
+<footer>
+</footer>
+</body>
+</html>
